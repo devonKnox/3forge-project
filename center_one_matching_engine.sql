9d0d8f20a1f80acaa448edfda3d70e97123da29b
Devon Knox <devon.knox@duke.edu>
2025-07-02 12:49:12 -0400

    Added some more features to matching engine, including timer and processing orders for multiple symbols from orderFeed. On AMI web, also added button for users to add their own trades to orderFeed.

--- center_one_matching_engine.sql
+++ center_one_matching_engine.sql
@@ -1,37 +1,55 @@
-// setting up center for matching engine and trade generation
 // // Add indexes later for fast SFW commands (definitely needed for HDB)
-
-// DROP TRIGGER IF EXISTS addOrdertoBook(String symbol, String direction, )
 //Add tables with "I" flag later for correct startup
-//CREATE TRIGGER TRIGGER_PROCESS_ORDERS OFTYPE AMISCRIPT ON MD1Snaps USE canMutateRow="false" onInsertedScript="int ret = processOrders(Symbol);";
-
 
 DROP TRIGGER IF EXISTS executeTrade;
-DROP TABLE IF EXISTS trades;
-DROP METHOD IF EXISTS matchOrder(String symbol, String direction, double price, Int qty, Int openQty, Int orderId, account String, kind String);
+DROP METHOD IF EXISTS simpleMatch(String symbol);
 
-CREATE PUBLIC TABLE trades(
-    symbol String,
-    quantity Int,
-    price double,
-    sellAccount String,
-    buyAccount String,
-    timestamp double
-);
+CREATE METHOD Int simpleMatch(String symbol)
+{  
+  //Get one BUY order
+  Table buyTable = USE ds="AMI" EXECUTE SELECT * FROM orderFeed WHERE direction == "BUY" AND OpenQty > 0 ORDER BY price DESC, timestamp ASC; // works
 
-CREATE TRIGGER executeTrade OFTYPE AMISCRIPT ON orderFeed USE onInsertedScript="int ret = matchOrder(symbol, orderId, account, direction, kind);";
+  // Get one SELL order
+  Table sellTable = USE ds="AMI" EXECUTE SELECT * FROM orderFeed WHERE direction == "SELL" AND OpenQty > 0 ORDER BY price ASC, timestamp ASC;
+  // Get rows
+  List buys = buyTable.getRows();
+  List sells = sellTable.getRows();
 
-//CREATE TRIGGER executeTrade OFTYPE AMISCRIPT ON orderFeed USE
-//rowVar = "_row"
-//onInsertedScript="insert into trades values(\"test\", 1, 1, \"test\", \"test\", 1)";
+  if (buys.size() == 0 && sells.size() == 0) return 0; // Don't think this is needed?
 
+  // Retrieve best buy and best sell
+  Map buy = buys.get(0);
+  Map sell = sells.get(0);
 
-// For later
-//if(${symbol} == "TSLA" && ${direction} == "BUY") {
-//insert into tslaBuyLadder(quantity, price, kind, timestamp) values(1, 0, "TS", \"${timestamp()}\");
-//}""";
+  Double buyPrice = buy.get("price");
+  Double sellPrice = sell.get("price");
 
+  if (sellPrice - buyPrice >= 20) {
+    return 0;
+  }
 
-//CREATE TRIGGER ordertoBook_tslaSellLadder OFTYPE AMISCRIPT ON orderFeed USE
-//onInsertedScript="insert into test_one(symbol, price) values("test", 100)";
-//insert into tslaBuyLadder(symbol, price) values("test", 100);
+  Int buyQty = buy.get("OpenQty");
+  String buyAcc = buy.get("Account");
+  Int sellQty = sell.get("OpenQty");
+  String sellAcc = sell.get("Account");
+  
+  
+  Int matchQty = minimum(buyQty, sellQty);
+  Int newOpenQty_buy = buyQty - matchQty;
+  Int newOpenQty_sell = sellQty - matchQty;
+  Double tradePrice = (buyPrice + sellPrice) / 2;
+  String stri = "acc";
+  Double dub = 5;
+  Int qt = 5;
+
+  USE ds="AMI" EXECUTE INSERT INTO trades(symbol, quantity, price, sellAccount, buyAccount, timestamp) VALUES ("${symbol}", ${matchQty}, ${tradePrice}, "${buyAcc}", "${sellAcc}", ${timestamp()} );
+
+  // Need to update openQty in order book for both buy and sells
+  USE ds="AMI" EXECUTE UPDATE orderFeed SET OpenQty == newOpenQty_buy WHERE account == "${buyAcc}"";
+  USE ds="AMI" EXECUTE UPDATE orderFeed SET OpenQty == newOpenQty_sell WHERE account == "${sellAcc}"";
+
+  
+  return 1;
+};
+String t = "TSLA";
+CREATE TRIGGER executeTrade OFTYPE AMISCRIPT ON orderFeed USE onInsertedScript="int ret = simpleMatch(t);";
